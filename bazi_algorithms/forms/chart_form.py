292607db7e880199742a8b9779ff9a2ac07b8f43
tonytonggg <tonytongwa@gmail.com>
2021-09-17 22:14:46 +0800

    added bazi api to account homepage

--- bazi_algorithms/forms/chart_form.py
+++ bazi_algorithms/forms/chart_form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from wtforms import validators, SelectField, SubmitField, StringField, BooleanField
+from datetime import datetime
+from wtforms.fields.html5 import DateField
 
-class ChartForm(FlaskForm):
+class ChartFormManual(FlaskForm):
 
     name = StringField('Name',validators=[DataRequired()])
 
@@ -16,5 +18,16 @@ class ChartForm(FlaskForm):
     month_branch = SelectField('Month Branch', [DataRequired()], choices=branch_choices)
     year_stem = SelectField('Year Stem', [DataRequired()], choices=stem_choices)
     year_branch = SelectField('Year Branch', [DataRequired()], choices=branch_choices)
-    my_own_chart_checkbox = BooleanField('This is My Chart')
+    my_own_chart_checkbox = BooleanField('(This is My Chart)')
     submit = SubmitField('Submit')
+
+class ChartFormBirthTime(FlaskForm):
+
+    name = StringField('Name',validators=[DataRequired()])
+    birth_date = DateField('Birthday', format='%Y-%m-%d', default=datetime(1985, 1, 30), validators=(DataRequired(),))
+    hour_choices = [("12am-1am",'12am-1am'),("1am-3am",'1am-3am'),("3am-5am",'3am-5am'),("5am-7am",'5am-7am'),("7am-9am",'7am-9am'),("9am-11am",'9am-11am'),("11am-1pm",'11am-1pm'),("1pm-3pm",'1pm-3pm'),('3pm-5pm','3pm-5pm'),("5pm-7pm",'5pm-7pm'),("7pm-9pm",'7pm-9pm'),("9pm-11pm",'9pm-11pm'),("11pm-12pm",'11pm-12pm')]
+    hour_range = SelectField('Hour Range', choices=[(None, "")] + hour_choices)
+    gender = SelectField('Gender', choices=[("False", "Female"), ("True", "Male")], validators=(DataRequired(),))
+    my_own_chart_checkbox = BooleanField('(This is My Chart)')
+    submit = SubmitField('Submit')
+
--- bazi_algorithms/routes.py
+++ bazi_algorithms/routes.py
@@ -2,6 +2,11 @@ from flask import Blueprint, redirect, render_template, url_for, flash, session,
 from flask_login import current_user, login_required
 from flask_wtf.csrf import CSRFProtect
 from .cache import cache
+from .forms.chart_form import ChartFormManual, ChartFormBirthTime
+from .persistence.models import NatalChart, db, User
+import requests
+import re
+from os import environ
 
 main_bp = Blueprint(
     'main_bp', __name__,
@@ -12,23 +17,59 @@ main_bp = Blueprint(
 @main_bp.route("/home", methods=['GET', 'POST'])
 @login_required
 def main():
-    from .forms.chart_form import ChartForm
-    from .persistence.models import NatalChart, db, User
-    chart_form = ChartForm()
+    chart_form = ChartFormManual()
+    chart_form_2 = ChartFormBirthTime()
 
-    if chart_form.validate_on_submit():
-        natal_chart = NatalChart(
-            user_id = current_user.id,
-            contact_name = chart_form.name.data,
-            hour_s = chart_form.hour_stem.data,
-            hour_e = chart_form.hour_branch.data,
-            day_s = chart_form.day_stem.data,
-            day_e = chart_form.day_branch.data,
-            month_s = chart_form.month_stem.data,
-            month_e = chart_form.month_branch.data,
-            year_s = chart_form.year_stem.data,
-            year_e = chart_form.year_branch.data
-        )
+    if request.method == 'POST':
+        if chart_form_2.validate_on_submit():
+
+            hour = re.search(r'[0-9]*', chart_form_2.hour_range.data).group()
+            birthday = str(chart_form_2.birth_date.data)
+            bazi_api_url = environ.get('BAZI_API_URL')
+            bazi_url = bazi_api_url.format(chart_form_2.gender.data, birthday[:4], birthday[5:7], birthday[8:11], hour,chart_form_2.hour_range.data[:-2])
+            page = requests.get(bazi_url)
+            content = str(page.content)
+
+            x = re.search("(?=Day Master)(.*)(?=10 Year)", content)
+            y = re.search("(?=<td>)(.*)(</td>)", x[0])
+            z = re.sub('<[^<]+?>', '', y[0])
+            z = re.sub(r'\\x*[A-z0-9]+', '', z)
+            z = z.replace("\\r", "").replace('\\n', "")
+            z = re.sub("[ ]+", ' ', z)
+            
+            hour = re.search("(?=Hour)(.*)(?=Day)", z)[0].split(" ")
+            day = re.search("(?=Day)(.*)(?=Month)", z)[0].split(" ")
+            month = re.search("(?=Month)(.*)(?=Year)", z)[0].split(" ")
+            year = re.search("(?=Year)(.*)", z)[0].split(" ")
+            print(z)
+            print(hour)
+
+            natal_chart = NatalChart(
+                user_id = current_user.id,
+                contact_name = chart_form_2.name.data,
+                hour_s = hour[1],
+                hour_e = hour[4],
+                day_s = day[1],
+                day_e = day[4],
+                month_s = month[1],
+                month_e = month[4],
+                year_s = year[1],
+                year_e = year[4]
+            )
+
+        if chart_form.validate_on_submit():
+            natal_chart = NatalChart(
+                user_id = current_user.id,
+                contact_name = chart_form.name.data,
+                hour_s = chart_form.hour_stem.data,
+                hour_e = chart_form.hour_branch.data,
+                day_s = chart_form.day_stem.data,
+                day_e = chart_form.day_branch.data,
+                month_s = chart_form.month_stem.data,
+                month_e = chart_form.month_branch.data,
+                year_s = chart_form.year_stem.data,
+                year_e = chart_form.year_branch.data
+            )
 
         chart_validation = NatalChart.query.filter_by(user_id=current_user.id, contact_name=chart_form.name.data).all()
         if len(chart_validation) > 0:
@@ -37,7 +78,7 @@ def main():
             try:
                 db.session.add(natal_chart)
                 db.session.commit() 
-                flash("Chart for " + chart_form.name.data + " added.", "info")
+                flash("Chart for " + chart_form.name.data + " added. Bazi: " + natal_chart.hour_s + " " + natal_chart.hour_e + ", " + natal_chart.day_s + " " +  natal_chart.day_e + ", " + natal_chart.month_s + " " + natal_chart.month_e + ", " + natal_chart.year_s + " " + natal_chart.year_e, "info")
             except Exception as e: 
                 db.session.rollback()
                 print(e)
@@ -47,10 +88,9 @@ def main():
                 new_natal_chart_id = NatalChart.query.filter_by(user_id=current_user.id, contact_name=chart_form.name.data).one().id
                 User.query.filter_by(id=current_user.id).update({"natal_chart_id": new_natal_chart_id})
                 db.session.commit() 
-                flash("Your personal chart is updated.", "info")
+                flash("Your personal chart is updated. Bazi: " + natal_chart.hour_s + " " + natal_chart.hour_e + ", " + natal_chart.day_s + " " +  natal_chart.day_e + ", " + natal_chart.month_s + " " + natal_chart.month_e + ", " + natal_chart.year_s + " " + natal_chart.year_e, "info")
 
     natal_chart_id = User.query.filter_by(id=current_user.id).one().natal_chart_id
-    print(NatalChart.query.filter_by(id=natal_chart_id).all())
     if NatalChart.query.filter_by(id=natal_chart_id).all() != []:
         user_chart = NatalChart.query.filter_by(id=natal_chart_id).one()
     else:
@@ -59,5 +99,6 @@ def main():
     return render_template('home.jinja2', 
     current_user = current_user,
     form = chart_form,
+    chart_form_birth_time = chart_form_2,
     user_chart = user_chart
     )
