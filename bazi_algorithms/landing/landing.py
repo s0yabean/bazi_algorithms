0862ddb203a55364f03530ffc86620f67df06e8f
tonytonggg <tonytongwa@gmail.com>
2021-10-02 11:37:39 +0800

    added logo and favicon

--- bazi_algorithms/landing/landing.py
+++ bazi_algorithms/landing/landing.py
@@ -12,19 +12,19 @@ landing_bp = Blueprint(
 def landing():
     return render_template(
         'landing.jinja2',
-        title='Bazilogy - Master Your Network.'
+        title="Leverage Your Network's Bazi."
     )
 
 @landing_bp.route('/about', methods=['GET'])
 def about_us():
     return render_template(
         'about.jinja2',
-        title='Bazilogy - Master Your Network.'
+        title='Bazi for Modern Age.'
     )
 
 @landing_bp.route('/newsletter', methods=['GET'])
 def newsletter():
     return render_template(
         'newsletter.jinja2',
-        title='Bazilogy - Master Your Network.'
+        title='Bazilogy Newsletter.'
     )
