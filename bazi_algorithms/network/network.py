613794ca04da65fe2eaefdc578d347be60a2ab3d
enix403 <fog.code000@gmail.com>
2021-12-09 11:21:08 +0000

    Changed timeline.py and network.py

--- bazi_algorithms/network/network.py
+++ bazi_algorithms/network/network.py
@@ -1,12 +1,20 @@
-from re import I, L
 from bazi_algorithms.persistence.models import NatalChart
-from flask import Blueprint, redirect, render_template, url_for, flash, session, request, abort
+from flask import (
+    Blueprint,
+    render_template,
+    flash,
+    session,
+    request
+)
 from flask_login import current_user, login_required
-from flask_wtf.csrf import CSRFProtect
-import pandas as pd
-from datetime import date
-import numpy as np
-from .. import db
+
+from ..forms.network_forms import NetworkForm_2
+from ..persistence.models import NatalChart
+from .explanations import explanation_list_paid, explanation_list_free
+
+from .questions_1 import career_plus
+from .questions_6_7 import nobleman_star, peach_blossom_me_to_others, peach_blossom_others_to_me
+from .questions_2_3_4_5 import easiest_sell, more_prod, more_reckless, more_rebellious
 
 network_bp = Blueprint(
     'network_bp', __name__,
@@ -17,12 +25,6 @@ network_bp = Blueprint(
 @network_bp.route("/network", methods=['GET', 'POST'])
 @login_required
 def main():
-    from ..forms.date_forms import DateForm
-    from ..forms.name_forms import NetworkForm
-    from ..forms.network_forms import NetworkForm_2
-    from ..persistence.models import NatalChart, ExternalPillars
-    from .explanations import explanation_list_paid, explanation_list_free
-
     table_data = NatalChart.query.filter_by(user_id=current_user.id, self_chart=False).all() 
     natal_chart_id = current_user.natal_chart_id
     remarks = []
@@ -31,7 +33,7 @@ def main():
     questions = question_list()
     user_chart = None
     total_other_ppl_charts =  0
-    NetworkForm_2 = NetworkForm_2()
+    netform_2 = NetworkForm_2()
 
     if request.method == 'GET':
         if current_user.natal_chart_id is None:
@@ -40,65 +42,91 @@ def main():
                 'network.jinja2',
                 current_user=current_user,
                 table_data = table_data,
-                network_form_2=NetworkForm_2,
-                questions=[],
-                remarks=[],
-                explanations=[],
-                user_chart=user_chart,
-                total_other_ppl_charts = total_other_ppl_charts)
+                network_form_2=netform_2,
 
-    if NetworkForm_2.validate_on_submit():
-        if NetworkForm_2.startdate.data is not None and NetworkForm_2.enddate.data is not None:
-            days_apart = (NetworkForm_2.enddate.data - NetworkForm_2.startdate.data).days
-            if days_apart <= 1:
-                flash(f"Date Range of {NetworkForm_2.startdate.data} to {NetworkForm_2.enddate.data} Needs To Be At Least 2 Days Apart. Current range: {days_apart} day.",  'error')
-                return render_template(
-                'network.jinja2',
-                current_user=current_user,
-                table_data = table_data,
-                network_form_2=NetworkForm_2,
-                questions=[],
-                remarks=[],
-                explanations=[],
+                questions=[], remarks=[], explanations=[],
+
                 user_chart=user_chart,
-                total_other_ppl_charts = total_other_ppl_charts)
+                total_other_ppl_charts = total_other_ppl_charts
+            )
+
+    if netform_2.validate_on_submit():
+        if netform_2.startdate.data is not None and netform_2.enddate.data is not None:
+            days_apart = (netform_2.enddate.data - netform_2.startdate.data).days
+            if days_apart <= 1:
+                
+                flash(f"Date Range of {netform_2.startdate.data} to {netform_2.enddate.data} Needs To Be At Least 2 Days Apart. "
+                       "Current range: {days_apart} day.",  'error')
+
+                return render_template(
+                    'network.jinja2',
+
+                    current_user=current_user,
+                    table_data=table_data,
+                    network_form_2=netform_2,
+
+                    questions=[], remarks=[], explanations=[],
+
+                    user_chart=user_chart,
+                    total_other_ppl_charts=total_other_ppl_charts
+                )
 
         show_success_flash = True
-        if NetworkForm_2.category.data is not None:
-            session['question'] = int(NetworkForm_2.category.data)
+        if netform_2.category.data is not None:
+            session['question'] = int(netform_2.category.data)
         
-        session['start_date'] = NetworkForm_2.startdate.data
-        session['end_date'] = NetworkForm_2.enddate.data
+        session['start_date'] = netform_2.startdate.data
+        session['end_date'] = netform_2.enddate.data
 
-    if 'question' in session.keys():
-        if session['question'] in [1,2,3,4]:
-            if ('start_date' not in session.keys() and 'end_date' not in session.keys()) or (session['start_date'] is None or session['end_date'] is None):
+    sess_question = session.get('question')
+
+    if sess_question is not None:
+        if sess_question in [1,2,3,4]:
+
+            sess_start_date = session.get('start_date', None)
+            sess_end_date = session.get('end_date', None)
+
+            remarks = []
+
+            if not sess_start_date or not sess_end_date:
                 flash("Please Select A Date Range", "error")
-                table_data, remarks = [], []
-            elif (session['start_date'] > session['end_date']):
-                flash(f"End Date ({session['end_date']}) cannot be before Start Date ({session['start_date']}). Please change date range.", "error")
-                table_data, remarks = [], []
+                table_data= []
+            elif sess_start_date > sess_end_date:
+                flash(f"End Date ({sess_end_date}) cannot be before Start Date ({sess_start_date}). "
+                       "Please change date range.", "error")
+                table_data = []
             else:
-                table_data, remarks = answer_question(questions[session['question']], table_data, natal_chart_id, session['start_date'], session['end_date'])
+                table_data, remarks = answer_question(
+                    questions[sess_question], 
+                    table_data, 
+                    natal_chart_id, 
+                    sess_start_date, 
+                    sess_end_date
+                )
                 
                 if show_success_flash:
                     flash("Calculation Success.","info")
-        elif session['question'] in [0,5,6,7]:
+
+
+        elif sess_question in [0,5,6,7]:
             if current_user.natal_chart_id is None:
                 flash("Please add in your own chart in the Account section for some questions to work.", "info")
                 flash("Personal chart not found, unable to answer question.", "error")
                 return render_template(
-                'network.jinja2',
-                current_user=current_user,
-                table_data = table_data,
-                network_form_2=NetworkForm_2,
-                questions=[],
-                remarks=[],
-                explanations=[],
-                user_chart=user_chart,
-                total_other_ppl_charts = total_other_ppl_charts)
+                    'network.jinja2',
 
-            table_data, remarks = answer_question(questions[session['question']], table_data, natal_chart_id, "", "")
+                    current_user=current_user,
+                    table_data = table_data,
+                    network_form_2=netform_2,
+
+                    questions=[], remarks=[], explanations=[],
+
+                    user_chart=user_chart,
+                    total_other_ppl_charts = total_other_ppl_charts
+                )
+
+            table_data, remarks = answer_question(questions[sess_question], table_data, natal_chart_id, "", "")
+
             if show_success_flash:
                 flash("Calculation Success.","info")
 
@@ -107,58 +135,64 @@ def main():
         else:
             explanations = explanation_list_paid()
 
-        for i in range(len(explanations)):
-            if questions[session['question']] == explanations[i][0][1]:
-                explanations = explanations[i]
+        for expl in explanations:
+            if questions[sess_question] == expl[0][1]:
+                explanations = expl
                 break
 
-        if NatalChart.query.filter_by(id=current_user.natal_chart_id).all() != []:
-            user_chart = NatalChart.query.filter_by(id=current_user.natal_chart_id).one()
+        db_natal_chart = NatalChart.query.filter_by(id=current_user.natal_chart_id).first()
+
+        if db_natal_chart is not None:
+            user_chart = db_natal_chart
             total_other_ppl_charts = NatalChart.query.filter_by(user_id=current_user.id, self_chart=False).count()
             print("total_other_ppl_charts")
             print(total_other_ppl_charts)
             
     return render_template(
         'network.jinja2',
+
         current_user=current_user,
         table_data = table_data,
-        network_form_2=NetworkForm_2,
+        network_form_2=netform_2,
+
         questions=questions,
         remarks=remarks,
         explanations=explanations,
-        user_chart=user_chart,
-        total_other_ppl_charts = total_other_ppl_charts)
 
-from ..bazi_formulas.combines import count_earth_combo, count_earth_six_harmony_combine
-from ..persistence.models import NatalChart, ExternalPillars
-from .questions_1 import career_plus
-from .questions_6_7 import nobleman_star, peach_blossom_me_to_others, peach_blossom_others_to_me
-from .questions_2_3_4_5 import easiest_sell, more_prod, more_reckless, more_rebellious
-def answer_question(input, table_data, natal_chart_id, start_date, end_date):
+        user_chart=user_chart,
+        total_other_ppl_charts = total_other_ppl_charts
+    )
+
+
+def answer_question(qs_input, table_data, natal_chart_id, start_date, end_date):
     ques = question_list()
-    if input == ques[0]:
+    if qs_input == ques[0]:
         return career_plus(table_data, natal_chart_id)
-    elif input == ques[1]:
+    elif qs_input == ques[1]:
         return easiest_sell(table_data, start_date, end_date)
-    elif input == ques[2]:
+    elif qs_input == ques[2]:
         return more_prod(table_data, start_date, end_date)
-    elif input == ques[3]:
+    elif qs_input == ques[3]:
         return more_reckless(table_data, start_date, end_date)
-    elif input == ques[4]:
+    elif qs_input == ques[4]:
         return more_rebellious(table_data, start_date, end_date)
-    elif input == ques[5]:
+    elif qs_input == ques[5]:
         return nobleman_star(table_data, natal_chart_id)
-    elif input == ques[6]:
+    elif qs_input == ques[6]:
         return peach_blossom_me_to_others(table_data, natal_chart_id)
-    elif input == ques[7]:
+    elif qs_input == ques[7]:
         return peach_blossom_others_to_me(table_data, natal_chart_id)
 
+__all_questions = [
+    "Who would be a good career partner/ colleague/ mentor?", 
+    "Who is easiest to sell to this period?",
+    "Who is more hardworking/ productive than normal this period?",
+    "Who is more reckless than normal this period?",
+    "Who is more rebellious than normal this period?",
+    "Who are my nobleman (贵人)?",
+    "Who am I more attracted to (has my peach blossom)?",
+    "Who is attracted to me (I have their peach blossom)?"
+]
+
 def question_list():
-    return ["Who would be a good career partner/ colleague/ mentor?", 
-                 "Who is easiest to sell to this period?",
-                 "Who is more hardworking/ productive than normal this period?",
-                 "Who is more reckless than normal this period?",
-                 "Who is more rebellious than normal this period?",
-                 "Who are my nobleman (贵人)?",
-                 "Who am I more attracted to (has my peach blossom)?",
-                 "Who is attracted to me (I have their peach blossom)?"]
+    return __all_questions
--- bazi_algorithms/timeline/timeline.py
+++ bazi_algorithms/timeline/timeline.py
@@ -1,12 +1,21 @@
-from flask import Blueprint, redirect, render_template, url_for, flash, session, request, abort
-from flask_login import current_user, login_required
-from flask_wtf.csrf import CSRFProtect
-import pandas as pd
-from datetime import date
 import math
+from datetime import date, datetime, timedelta
+from flask import (
+    Blueprint,
+    render_template,
+    flash,
+    session, 
+    request
+)
+from flask_login import current_user, login_required
+
+import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta
-import logging
+
+from bazi_algorithms.forms.date_forms import TimelineDateForm
+from bazi_algorithms.forms.name_forms import ChoiceForm
+from bazi_algorithms.persistence.models import NatalChart, ExternalPillars
+from bazi_algorithms.bazi_formulas.timeline_scoring import calc_comb_clash
 
 timeline_bp = Blueprint(
     'timeline_bp', __name__,
@@ -17,11 +26,7 @@ timeline_bp = Blueprint(
 @timeline_bp.route("/timeline", methods=['GET', 'POST'])
 @login_required
 def main():
-    from ..forms.date_forms import TimelineDateForm
-    from ..forms.name_forms import ChoiceForm
-    from ..persistence.models import NatalChart, ExternalPillars
-    from ..bazi_formulas.timeline_scoring import calc_comb_clash
-    
+
     data = [{'s_combine': 0, 's_clash': 0, 'e_combine': 0, 'e_clash': 0}]
     avg_combine = [0] 
     avg_clash = [0]
@@ -31,72 +36,120 @@ def main():
     chart = None
     dates = ExternalPillars.query.filter(ExternalPillars.date == str(date.today())).all()
 
-    natal_chart_list = NatalChart.query.filter_by(user_id=current_user.id).all()
-    ChoiceForm = ChoiceForm()
+    natal_chart_list = list(NatalChart.query.filter_by(user_id=current_user.id).all())
 
-    Dateform = TimelineDateForm()
+    choice_form = ChoiceForm()
+    date_form = TimelineDateForm()
 
     if 'start_date' not in session.keys():
         session['start_date'] = datetime(int(str(datetime.today())[:4]), 1, 1)
         session['end_date'] = datetime(int(str(datetime.today())[:4]), 12, 31)
-        dates = ExternalPillars.query.filter(ExternalPillars.date >= session["start_date"] - timedelta(days=window_size), ExternalPillars.date <= session["end_date"]).all()
+        dates = ExternalPillars.query.filter(
+                    ExternalPillars.date >= session["start_date"] - timedelta(days=window_size), 
+                    ExternalPillars.date <= session["end_date"]
+                ).all()
 
     if request.method == 'GET':
         if current_user.natal_chart_id is None:
             flash("Please add in your own chart in the Account section to see your own chart on default.", "info")
-            return render_template('timeline.jinja2', form=Dateform, user_chart=chart,
-                natal_chart=natal_chart_list, choice_form=ChoiceForm,
-                session=session, data=data[window_size:], labels=[d.date for d in dates[window_size:]],
-                avg_combine=avg_combine,avg_clash= avg_clash, rolling_data=rolling_data_comb[window_size:], rolling_data_clash=rolling_data_clash[window_size:])
+            return render_template('timeline.jinja2', 
+                form=date_form,
+                user_chart=chart,
+                natal_chart=natal_chart_list,
+                choice_form=choice_form,
+                session=session,
 
-        if "contact_chart" not in session.keys() or ("contact_chart" in session.keys() and session['contact_chart'] is None):    
+                data=data[window_size:],
+                labels=[d.date for d in dates[window_size:]],
+
+                avg_combine=avg_combine,
+                avg_clash=avg_clash,
+
+                rolling_data=rolling_data_comb[window_size:],
+                rolling_data_clash=rolling_data_clash[window_size:]
+            )
+
+        if session.get('contact_chart') is None:
             default_natal_chart = NatalChart.query.filter_by(id=current_user.natal_chart_id).one()
             session['contact_name_id'] = default_natal_chart.id
             session['contact_name'] = default_natal_chart.contact_name
 
-    if request.method == 'POST':
-        if "dropdown_menu" and "security_token" in request.form.keys():
+    elif request.method == 'POST':
+        if "dropdown_menu" in request.form.keys() and "security_token" in request.form.keys():
+            # The value of request.form['security_token'] should probably be validated here
             contact_id = request.form["dropdown_menu"]
             contact_chart = NatalChart.query.filter_by(id=contact_id).one()
+
             session['contact_name_id'] = contact_id 
             session['contact_name'] = contact_chart.contact_name
             session["contact_chart"] = contact_chart
-            Dateform.startdate.data = session['start_date']
-            Dateform.enddate.data = session['end_date']
+
+            date_form.startdate.data = session['start_date']
+            date_form.enddate.data = session['end_date']
     
     print("outside form submit")
-    if Dateform.validate_on_submit():
+    if date_form.validate_on_submit():
         print("entered date form")
-        print(Dateform.startdate.data)
-        print(Dateform.enddate.data)
-        session['start_date'] = Dateform.startdate.data
-        session['end_date'] = Dateform.enddate.data
+        print(date_form.startdate.data)
+        print(date_form.enddate.data)
+        session['start_date'] = date_form.startdate.data
+        session['end_date'] = date_form.enddate.data
         print(session['start_date'])
         print(session['end_date'])
 
-    if 'contact_name' not in session.keys() and 'contact_name_id' not in session.keys():
+    contact_not_present =   'contact_name' not in session.keys() and \
+                            'contact_name_id' not in session.keys() 
+
+    if contact_not_present:
         flash("Please Select A Person In The Dropdown Menu.", "info")
 
-    display_chart = 'start_date' in session.keys() and 'end_date' in session.keys() and session["start_date"] is not None and session["end_date"] is not None and 'contact_name' in session.keys() and 'contact_name_id' in session.keys()
+    display_chart = 'start_date' in session.keys() and \
+                    'end_date' in session.keys() and \
+                    session["start_date"] is not None and \
+                    session["end_date"] is not None and \
+                    not contact_not_present
+
     if display_chart:
-        num_days = ExternalPillars.query.filter(ExternalPillars.date >= session["start_date"], ExternalPillars.date <= session["end_date"]).count()
+
+        num_days = ExternalPillars.query.filter(
+            ExternalPillars.date >= session["start_date"],
+            ExternalPillars.date <= session["end_date"]
+        ).count()
+
         window_size = math.ceil(num_days/ 25)
-        dates = ExternalPillars.query.filter(ExternalPillars.date >= session["start_date"] - timedelta(days=window_size), ExternalPillars.date <= session["end_date"]).all()
+
+        dates = ExternalPillars.query.filter(
+            ExternalPillars.date >= session["start_date"] - timedelta(days=window_size),
+            ExternalPillars.date <= session["end_date"]
+        ).all()
+
         chart = NatalChart.query.filter_by(id=session["contact_name_id"]).one()
         n_chart_stems = [chart.hour_s, chart.day_s, chart.month_s, chart.year_s] 
         n_chart_branches = [chart.hour_e, chart.day_e, chart.month_e, chart.year_e]
         data = calc_comb_clash(n_chart_stems, n_chart_branches, dates)
+
         comb_list = [e["e_combine"] for e in data]
         clash_list = [e["e_clash"] for e in data]
+
         avg_combine = [round(np.mean(comb_list), 2)] * len(data)
         avg_clash = [round(np.mean(comb_list), 2)] * len(data)
+
         rolling_data_comb = comb_list[:window_size - 1] + rolling_avg(comb_list, window_size)
         rolling_data_clash = comb_list[:window_size - 1] + rolling_avg(clash_list, window_size)
 
-    return render_template('timeline.jinja2', form=Dateform, user_chart=chart,
-    natal_chart=natal_chart_list, choice_form=ChoiceForm,
-    session=session, data=data[window_size:], labels=[d.date for d in dates[window_size:]],
-    avg_combine=avg_combine,avg_clash= avg_clash, rolling_data=rolling_data_comb[window_size:], rolling_data_clash=rolling_data_clash[window_size:])
+    return render_template('timeline.jinja2', 
+        form=date_form,
+        user_chart=chart,
+        natal_chart=natal_chart_list,
+        choice_form=choice_form,
+        session=session,
+        data=data[window_size:],
+        labels=[d.date for d in dates[window_size:]],
+        avg_combine=avg_combine,
+        avg_clash=avg_clash,
+        rolling_data=rolling_data_comb[window_size:],
+        rolling_data_clash=rolling_data_clash[window_size:]
+    )
 
 def rolling_avg(data, window_size):
     numbers_series = pd.Series(data)
